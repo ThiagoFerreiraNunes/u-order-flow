3e318048de1086e6b2f262ab87353e7e9ec56a47
ThiagoFerreiraNunes <thiagonflive@gmail.com>
2026-05-04 20:38:37 -0300

    chore: Configurando arquivos para o spring boot

---
+++ back-end/src/test/java/org/uorderflow/UorderflowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uorderflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UorderflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
