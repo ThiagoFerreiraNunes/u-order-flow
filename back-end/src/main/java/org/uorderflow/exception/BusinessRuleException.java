927225f176aca03c3e9c2cbab003595fcacfa54b
ThiagoFerreiraNunes <thiagonflive@exemplo.com>
2026-05-11 21:37:19 -0300

    criando classe para exception de regra de negócios

---
+++ back-end/src/main/java/org/uorderflow/exception/BusinessRuleException.java
@@ -0,0 +1,7 @@
+package org.uorderflow.exception;
+
+public class BusinessRuleException extends RuntimeException {
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
