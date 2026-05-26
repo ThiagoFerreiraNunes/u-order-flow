009907698027b1eebb4f714dd48e3d2cf6db9b77
ThiagoFerreiraNunes <thiagonflive@gmail.com>
2026-05-26 09:27:40 -0300

    feat: Adicionando o model para os alimentos

---
+++ back-end/src/main/java/org/uorderflow/model/Food.java
@@ -0,0 +1,64 @@
+package org.uorderflow.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.uorderflow.dto.food.FoodCreateDTO;
+import org.uorderflow.dto.food.FoodUpdateDTO;
+
+@Table(name = "tb_foods")
+@Entity(name = "Food")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Food {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "food_id")
+    private Long id;
+
+    @Column(name = "food_name")
+    private String name;
+
+    @Column(name = "food_description")
+    private String description;
+
+    @Column(name = "image_url")
+    private String image;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "is_available")
+    private Boolean isAvailable;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_category_id")
+    private FoodCategory foodCategory;
+
+    public Food(FoodCreateDTO data, FoodCategory foodCategory){
+        this.name = data.name();
+        this.description = data.description();
+        this.image = data.image();
+        this.price = data.price();
+        this.foodCategory = foodCategory;
+        this.isAvailable = true;
+    }
+
+    public void update(FoodUpdateDTO data, FoodCategory foodCategory){
+        if(data.name() != null) this.name = data.name();
+        if(data.description() != null) this.description = data.description();
+        if(data.image() != null) this.image = data.image();
+        if(data.price() != null) this.price = data.price();
+        if(foodCategory != null) this.foodCategory = foodCategory;
+    }
+
+    public void delete(){
+        this.isAvailable = false;
+    }
+
+    public void reactivate(){
+        this.isAvailable = true;
+    }
+}
