1c466c8cd41a7600bfe8a380ee49d686548ce879
ThiagoFerreiraNunes <thiagonflive@gmail.com>
2026-06-01 23:44:11 -0300

    feat: Adicionando o model para as mesas do restaurante

---
+++ back-end/src/main/java/org/uorderflow/model/RestaurantTable.java
@@ -0,0 +1,42 @@
+package org.uorderflow.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.uorderflow.dto.restaurantTable.RestaurantTableCreateDTO;
+import org.uorderflow.dto.restaurantTable.RestaurantTableUpdateDTO;
+
+@Table(name = "tb_restaurant_tables")
+@Entity(name = "RestaurantTable")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RestaurantTable {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "restaurant_table_id")
+    private Long id;
+
+    @Column(name = "restaurant_table_number")
+    private Integer number;
+
+    @Column(name = "is_available")
+    private Boolean isAvailable;
+
+    public RestaurantTable(RestaurantTableCreateDTO data){
+        this.number = data.number();
+    }
+
+    public void update(RestaurantTableUpdateDTO data){
+        if(data.number() != null) this.number = data.number();
+    }
+
+    public void delete(){
+        this.isAvailable = false;
+    }
+
+    public void reactivate(){
+        this.isAvailable = true;
+    }
+}
