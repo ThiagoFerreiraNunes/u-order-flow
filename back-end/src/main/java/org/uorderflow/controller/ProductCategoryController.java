fd1119f42247912c60cde531778a3cb38ae70ed8
ThiagoFerreiraNunes <thiagonflive@gmail.com>
2026-09-02 22:49:14 -0300

    refactor: Modificando o método GET de listagem de categoria de produtos para que os dados venham paginados

--- back-end/src/main/java/org/uorderflow/controller/ProductCategoryController.java
+++ back-end/src/main/java/org/uorderflow/controller/ProductCategoryController.java
@@ -2,6 +2,10 @@ package org.uorderflow.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -28,8 +32,8 @@ public class ProductCategoryController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductCategoryResponseDTO>> findAll(){
-        return ResponseEntity.ok(productCategoryService.findAll());
+    public ResponseEntity<Page<ProductCategoryResponseDTO>> findAll(@PageableDefault(page = 0, size = 20, sort = "name", direction = Sort.Direction.ASC) Pageable pageable){
+        return ResponseEntity.ok(productCategoryService.findAll(pageable));
     }
 
     @GetMapping("/{id}")
