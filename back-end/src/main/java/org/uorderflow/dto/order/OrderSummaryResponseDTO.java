55ff4e7de5196d4e7758d5fe69f6bf3bd0eb0177
ThiagoFerreiraNunes <thiagonflive@gmail.com>
2026-09-02 23:26:22 -0300

    refactor: Removendo a listagem de itens do pedido e adicionando o status do pedido em OderDummaryResponseDTO

--- back-end/src/main/java/org/uorderflow/dto/order/OrderSummaryResponseDTO.java
+++ back-end/src/main/java/org/uorderflow/dto/order/OrderSummaryResponseDTO.java
@@ -1,27 +1,24 @@
 package org.uorderflow.dto.order;
 
-import org.uorderflow.dto.orderProduct.OrderProductResponseDTO;
 import org.uorderflow.model.Order;
 import org.uorderflow.utils.FormatUtils;
 
-import java.util.List;
-
 public record OrderSummaryResponseDTO(
         Long id,
+        String status,
         String createdAt,
         String customer,
         String waiter,
-        Integer restaurantTable,
-        List<OrderProductResponseDTO> items
+        Integer restaurantTable
 ) {
     public OrderSummaryResponseDTO(Order order){
         this(
                 order.getId(),
+                order.getStatus().getDescription(),
                 FormatUtils.formatDateTime(order.getCreatedAt()),
                 order.getBill().getCustomer(),
                 order.getWaiter().getName(),
-                order.getBill().getRestaurantTable().getNumber(),
-                order.getItems().stream().map(OrderProductResponseDTO::new).toList()
+                order.getBill().getRestaurantTable().getNumber()
         );
     }
 }
