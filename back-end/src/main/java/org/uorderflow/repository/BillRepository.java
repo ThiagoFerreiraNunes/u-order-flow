14f5cccc5683b0b9fcf89b02634b5a012d9da33a
ThiagoFerreiraNunes <thiagonflive@gmail.com>
2026-09-05 14:40:37 -0300

    feat: Foi adicionada a funcionalidade para buscar Bills por data

--- back-end/src/main/java/org/uorderflow/repository/BillRepository.java
+++ back-end/src/main/java/org/uorderflow/repository/BillRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.uorderflow.model.Bill;
 
-
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface BillRepository extends JpaRepository<Bill, Long> {
@@ -22,4 +22,19 @@ public interface BillRepository extends JpaRepository<Bill, Long> {
             "LEFT JOIN FETCH b.orders " +
             "WHERE b.id = :id")
     Optional<Bill> findByIdWithDetails(@Param("id") Long id);
+
+    @Query(
+            value = "SELECT b FROM Bill b " +
+                    "JOIN FETCH b.restaurantTable " +
+                    "WHERE b.isDeleted = false " +
+                    "AND b.createdAt BETWEEN :startOfDay AND :endOfDay",
+            countQuery = "SELECT COUNT(b) FROM Bill b " +
+                    "WHERE b.isDeleted = false " +
+                    "AND b.createdAt BETWEEN :startOfDay AND :endOfDay"
+    )
+    Page<Bill> searchAllByDate(
+            @Param("startOfDay")LocalDateTime startOfDay,
+            @Param("endOfDay") LocalDateTime endOfDay,
+            Pageable pageable
+    );
 }
