8470345b055c8d27711a43e6a9664aa295116e4b
ThiagoFerreiraNunes <thiagonflive@gmail.com>
2026-09-08 14:30:55 -0300

    feat: Foi adicionada uma funcionalidade para ediar usuários, tendo um método específico para editar campos de name, email e role, e outro somente para editar a senha

--- back-end/src/main/java/org/uorderflow/service/user/UserService.java
+++ back-end/src/main/java/org/uorderflow/service/user/UserService.java
@@ -3,9 +3,13 @@ package org.uorderflow.service.user;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.uorderflow.dto.user.UserPasswordUpdateDTO;
 import org.uorderflow.dto.user.UserResponseDTO;
+import org.uorderflow.dto.user.UserUpdateDTO;
 import org.uorderflow.enums.user.UserAction;
 import org.uorderflow.enums.user.UserRole;
 import org.uorderflow.model.User;
@@ -18,10 +22,12 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserValidation userValidation;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, UserValidation userValidation) {
+    public UserService(UserRepository userRepository, UserValidation userValidation, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userValidation = userValidation;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional(readOnly = true)
@@ -52,6 +58,36 @@ public class UserService {
             return new UserResponseDTO(user);
         }
         throw new AccessDeniedException("Users with the " + loggedUser.getRole() + " role can only use the findById method with their own ids.");
+    }
+
+    @Transactional
+    public UserResponseDTO update(Long id, UserUpdateDTO data) {
+        User user = userValidation.validateUser(id, UserAction.ACTIVE_CHECK);
+
+        if(data.email() != null && !data.email().equals(user.getEmail())){
+            userValidation.validateUniqueFields(data.email());
+        }
+
+        user.update(data);
+        return new UserResponseDTO(user);
+    }
+
+    @Transactional
+    public void updatePassword(Long id, UserPasswordUpdateDTO data, User loggedUser) {
+        if (loggedUser.getRole() != UserRole.ADMIN && !Objects.equals(loggedUser.getId(), id)) {
+            throw new AccessDeniedException("Users with the " + loggedUser.getRole() + " role can only update their own passwords.");
+        }
+
+        User user = userValidation.validateUser(id, UserAction.ACTIVE_CHECK);
+
+        if (loggedUser.getRole() != UserRole.ADMIN) {
+            if (!passwordEncoder.matches(data.currentPassword(), user.getPassword())) {
+                throw new BadCredentialsException("The current password provided is incorrect.");
+            }
+        }
+
+        String encryptedPassword = passwordEncoder.encode(data.newPassword());
+        user.updatePassword(encryptedPassword);
     }
 
     @Transactional
