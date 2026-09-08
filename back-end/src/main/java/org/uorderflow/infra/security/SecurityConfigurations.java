2ecc896e54494b578039e1e9531bf2a07ec7399b
ThiagoFerreiraNunes <thiagonflive@gmail.com>
2026-09-07 23:03:36 -0300

    refactor: Na classe de SecurityConfigurations, as permissões para rotas foram refatoradas

--- back-end/src/main/java/org/uorderflow/infra/security/SecurityConfigurations.java
+++ back-end/src/main/java/org/uorderflow/infra/security/SecurityConfigurations.java
@@ -33,31 +33,42 @@ public class SecurityConfigurations {
                     authorize.requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll();
                     authorize.requestMatchers(HttpMethod.POST, "/api/auth/register").hasRole("ADMIN");
 
-                    // Users
-                    authorize.requestMatchers("/api/users/**").hasRole("ADMIN");
-
-                    // Product Categories
+                    // ProductCategories
                     authorize.requestMatchers("/api/product-categories/**").hasRole("ADMIN");
 
                     // Products
                     authorize.requestMatchers(HttpMethod.GET, "/api/products/**").hasAnyRole("ADMIN", "WAITER", "CASHIER", "COOK");
                     authorize.requestMatchers("/api/products/**").hasRole("ADMIN");
 
-                    // Restaurant Tables
-                    authorize.requestMatchers(HttpMethod.GET, "/api/restaurant-tables/**").hasAnyRole("ADMIN", "WAITER", "CASHIER", "COOK");
+                    // RestaurantTables
+                    authorize.requestMatchers(HttpMethod.GET, "/api/restaurant-tables/**").hasAnyRole("ADMIN", "WAITER", "CASHIER");
                     authorize.requestMatchers("/api/restaurant-tables/**").hasRole("ADMIN");
 
+                    // Users
+                    authorize.requestMatchers(HttpMethod.GET, "/api/users").hasAnyRole("ADMIN", "WAITER", "CASHIER");
+                    authorize.requestMatchers(HttpMethod.GET, "/api/users/*").hasAnyRole("ADMIN", "WAITER", "CASHIER", "COOK");
+                    authorize.requestMatchers("/api/users/**").hasRole("ADMIN");
+
                     // Bills
-                    authorize.requestMatchers(HttpMethod.GET, "/api/bills/**").hasAnyRole("ADMIN", "WAITER", "CASHIER", "COOK");
-                    authorize.requestMatchers("/api/bills/**").hasAnyRole("ADMIN", "WAITER", "CASHIER");
+                    authorize.requestMatchers(HttpMethod.PATCH,
+                            "/api/bills/*/cancel",
+                            "/api/bills/*/close",
+                            "/api/bills/*/pay").hasAnyRole("ADMIN", "CASHIER");
+                    authorize.requestMatchers(HttpMethod.POST, "/api/bills", "/api/bills/*/orders").hasAnyRole("ADMIN", "WAITER", "CASHIER");
+                    authorize.requestMatchers(HttpMethod.PUT, "/api/bills/*").hasAnyRole("ADMIN", "WAITER", "CASHIER");
+                    authorize.requestMatchers(HttpMethod.GET, "/api/bills/**").hasAnyRole("ADMIN", "WAITER", "CASHIER");
+                    authorize.requestMatchers("/api/bills/**").hasRole("ADMIN");
 
                     // Orders
-                    authorize.requestMatchers(HttpMethod.GET, "/api/orders/**").hasAnyRole("ADMIN", "WAITER", "CASHIER", "COOK");
                     authorize.requestMatchers(HttpMethod.PATCH, "/api/orders/*/prepare").hasAnyRole("ADMIN", "WAITER", "CASHIER", "COOK");
-                    authorize.requestMatchers("/api/orders/**").hasAnyRole("ADMIN", "WAITER", "CASHIER");
+                    authorize.requestMatchers(HttpMethod.GET, "/api/orders/**").hasAnyRole("ADMIN", "WAITER", "CASHIER", "COOK");
+                    authorize.requestMatchers(HttpMethod.PATCH,
+                            "/api/orders/*/cancel",
+                            "/api/orders/*/deliver").hasAnyRole("ADMIN", "WAITER", "CASHIER");
+                    authorize.requestMatchers(HttpMethod.PUT, "/api/orders/*").hasAnyRole("ADMIN", "WAITER", "CASHIER");
+                    authorize.requestMatchers("/api/orders/**").hasRole("ADMIN");
 
                     authorize.anyRequest().authenticated();
-
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
